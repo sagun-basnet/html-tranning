9d7994b732cb1750a19a20c54187b723220357b4
sagun basnet <basnetsagun8@gmail.com>
2024-07-31 12:31:38 +0545

    js DOM

---
+++ new folder/form.js
@@ -0,0 +1,16 @@
+// alert("");
+const handleChange = () => {
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+
+  console.log("Email: ", email.value);
+  console.log("Password: ", password.value);
+};
+const handleSubmit = () => {
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+
+  console.log("Email: ", email.value);
+  console.log("Password: ", password.value);
+};
+document.getElementById("mydiv").innerHTML = "<button>Click</button>";
